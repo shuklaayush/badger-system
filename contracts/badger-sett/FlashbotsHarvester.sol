46478fc7fa3c6e7046e3e6e21439de5229e2edfc
Ayush Shukla <shuklaayush247@gmail.com>
2021-08-11 03:51:57 +0530

    feat: add flashbots harvester deploy script

--- contracts/badger-sett/FlashbotsHarvester.sol
+++ contracts/badger-sett/FlashbotsHarvester.sol
@@ -7,32 +7,28 @@ import "deps/@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeabl
 import "interfaces/badger/IKeeperAccessControl.sol";
 
 contract FlashbotsHarvester is AccessControlUpgradeable, ReentrancyGuardUpgradeable {
-    bytes32 public constant KEEPER_ROLE = keccak256("KEEPER_ROLE");
+    bytes32 public constant HARVESTER_ROLE = keccak256("HARVESTER_ROLE");
 
     IKeeperAccessControl keeperAcl;
 
     function initialize(
         address admin,
-        address initialKeeper,
+        address initialHarvester,
         address _keeperAcl
     ) public initializer {
         __AccessControl_init();
 
         _setupRole(DEFAULT_ADMIN_ROLE, admin);
-        _setupRole(KEEPER_ROLE, initialKeeper);
+        _setupRole(HARVESTER_ROLE, initialHarvester);
         keeperAcl = IKeeperAccessControl(_keeperAcl);
     }
 
     /// ===== Modifiers =====
 
-    function _onlyKeeper() internal view {
-        require(hasRole(KEEPER_ROLE, msg.sender), "KEEPER_ROLE");
-    }
-
     // * @param strategy Address of strategy to harvest
     // * @param minHarvestedExpected Min amount of want expected to be harvested
     function harvest(address strategy, uint256 minHarvestExpected) external payable nonReentrant {
-        _onlyKeeper();
+        require(hasRole(HARVESTER_ROLE, msg.sender), "HARVESTER_ROLE");
 
         uint256 harvested = keeperAcl.harvest(strategy);
         require(harvested >= minHarvestExpected, "Harvest is too low");
